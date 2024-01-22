231af12b2e91a2a101fa63940b56cf989c03e462
Martin LoFaso <marty@Martins-Air.attlocal.net>
2024-01-22 15:13:11 -0800

    js edits

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,6 @@
 // - GIVEN I am taking a code quiz
 // - WHEN I click the start button
 // - THEN a timer starts and I am presented with a question
-// TO-DO: Create a landing page, timer, and a start button for the quiz, have quiz and timer begin when start button clicked
 
 //  Start Button
 var startButton = document.querySelector("#startButton")
@@ -35,17 +34,13 @@ function startTimer() {
 
 // - WHEN I answer a question
 // - THEN I am presented with another question
-// TO-DO: Link pages together so that answering a question brings you to the next page
 
 // - WHEN I answer a question incorrectly
-// - THEN time is subtracted from the clock
-// TO-DO: Link incorrect answers with decrementing the timer
+// - THEN time is subtracted from the clock - decrement
 
 // - WHEN all questions are answered or the timer reaches 0
-// - THEN the game is over
-// TO-DO: Terminate functions when timer reaches 0 or all questions answered
+// - THEN the game is over - clearInterval
 
 // - WHEN the game is over
-// - THEN I can save my initials and score
-// TO-DO: Create the page for entering their info and link entries to local storage
+// - THEN I can save my initials and score - Local Storage
 
